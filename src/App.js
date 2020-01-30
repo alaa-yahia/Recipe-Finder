673983015c7d6fa349941a92c52ebadc6630f7d4
Alaa Yahia <alaay873@live.com>
2020-01-30 12:29:22 +0200

    Getting data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const appId = '5e2bfbae';
+  const appKey = '08a8bc2749ab58dc89085f5d6a95df5e';
+  const [recipeName, setRecipeName] = useState('');
+  const [recipeDetails, setRecipeDetails] = useState('');
+
+  console.log(recipeDetails);
+  function handleClick() {
+    fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${appId}&app_key=${appKey}`)
+    .then(data => data.json())
+    .then((data) => setRecipeDetails(data));
+
+  }
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setRecipeName(value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' value={recipeName} onChange={handleChange}/>
+      <button onClick={handleClick}>Search</button>
     </div>
   );
 }
