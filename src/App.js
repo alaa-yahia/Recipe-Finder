42c78c660b6cb6472884d28664d409d2d62af8cd
Alaa Yahia <alaay873@live.com>
2020-02-12 14:45:14 +0200

    dd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, {useState} from 'react'
 import './App.css';
-
+//
 function App() {
   const appId = '5e2bfbae';
   const appKey = '08a8bc2749ab58dc89085f5d6a95df5e';
-  const [recipeName, setRecipeName] = useState('');
-  const [recipeDetails, setRecipeDetails] = useState('');
 
-  console.log(recipeDetails);
+  const [recipeName, setRecipeName] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const [start, setStart] = useState(0);
+  console.log(start);
+  function getRecipes(){
+    fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${appId}&app_key=${appKey}&from=${start}`)
+    .then(response => response.json())
+    .then((json) => {
+      console.log(json);
+      const value = [...recipes, ...json.hits];
+      setRecipes(value);
+      setStart(start+10);
+    }
+    );
+  }
   function handleClick() {
-    fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${appId}&app_key=${appKey}`)
-    .then(data => data.json())
-    .then((data) => setRecipeDetails(data));
+    getRecipes();
 
   }
 
@@ -21,8 +31,21 @@ function App() {
   }
   return (
     <div className="App">
-      <input type='text' value={recipeName} onChange={handleChange}/>
-      <button onClick={handleClick}>Search</button>
+      <form className="form">
+        <input type='text' value={recipeName} onChange={handleChange}/>
+        <button onClick={handleClick} type="button">Search</button>
+      </form>
+      <div className="recipes">
+        { recipes.map((item, index) => (
+            <div className="recipe" key={index}>
+              <img src={item.recipe.image} alt={item.recipe.label}/>
+              <p>{item.recipe.label}</p>
+            </div>
+            )
+          ) 
+        }
+      </div>
+      <p onClick={() => { getRecipes();}}>Loading More</p>
     </div>
   );
 }
